fa187dd4b4f701138140624ae816dda828da8ef8
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-12-28 15:34:46 -0400

    feat: add nodes to eks module

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -63,4 +63,55 @@ resource "aws_eks_cluster" "cluster" {
   ]
 }
 
+resource "aws_iam_role" "node" {
+  name               = "${var.prefix}-${var.cluster_name}-role-node"
+  assume_role_policy = <<POLICY
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "ec2.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }
+    POLICY
+}
 
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_node_group" "node-1" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node-1"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = ["t2.micro"]
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly
+  ]
+}
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,3 +3,6 @@ variable "subnet_ids" {}
 variable "prefix" {}
 variable "cluster_name" {}
 variable "retention_days" {}
+variable "desired_size" {}
+variable "max_size" {}
+variable "min_size" {}
